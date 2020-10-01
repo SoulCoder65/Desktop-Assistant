a3b7eb708ef73ff0c2f37b8f7f43a17dd1474e60
Ram Bhardwaj <63110845+Programmer-Sidharth@users.noreply.github.com>
2020-10-01 10:10:44 +0530

    Added the option to search the query on google similar to google assistant if nothing happens...

--- jarvis.py
+++ jarvis.py
@@ -125,3 +125,5 @@ if __name__ == '__main__':
                         speak_jarvis(hind_text)
                     except Exception as E:
                         speak_jarvis("problem")
+                else:
+                    webbrowser.open('https://www.google.com/search?q='+query)
